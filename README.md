8c08b9effae976bfb5e3e131f2138ed36f3b7009
Amirali.T <kebritam@proton.me>
2024-02-19 14:06:39 +0330

    @@ change jtop calls to support Jetson Nano @@ change grafana dashboard @@ change README.md, LICENSE.txt, grafana_dashboard_panel.png and requirments.txt

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # jetson-stats-grafana-dashboard
 
 ## Introduction
-This project is a Grafana dashboard, driven by a Prometheus metrics collector, for monitoring NVIDIA Jetson devices [Xavier NX, Nano, AGX Xavier, TX1, TX2] operating autonomously in a `headless` fashion (i.e. no connected display and associated user-input devices).
+This project is a Grafana dashboard, driven by a Prometheus metrics collector, for monitoring NVIDIA Jetson Nano operating autonomously in a headless fashion.
 
 ![image info](./grafana_dashboard_panel.png)
 
----
 
 ## Prerequisites
 
@@ -15,52 +14,20 @@ The following hardware and software should already be configured and operational
 - A host running [Grafana](https://grafana.com/) 
 - A host running [Prometheus](https://prometheus.io/)
 
----
 
 ## Installation
 
 ### Jetson Device
 
-1. Ensure `jetson_stats` is installed on your Jetson device. Full instructions can be found on the project [GitHub repo](https://github.com/rbonghi/jetson_stats)
+1. Install dependencies in 'requirments.txt' file
 
-2. Next, install the Prometheus metrics collector
-
-	```bash
-	nvidia@agx-desktop $ pip install prometheus-client
-	nvidia@agx-desktop $ sudo cp jetson_stats_prometheus_collector.py /usr/local/bin/
-	```
-
-3. Install the Prometheus metrics collector as a system background `systemd` service
+2. Next, install the collector with 'install.sh' bash script
 
 	```bash
-	nvidia@agx-desktop $ sudo cp jetson_stats_prometheus_collector.service /etc/systemd/system/
-	nvidia@agx-desktop $ sudo systemctl daemon-reload
-	nvidia@agx-desktop $ sudo systemctl start jetson_stats_prometheus_collector
-	nvidia@agx-desktop $ sudo systemctl status jetson_stats_prometheus_collector
+	nvidia@agx-desktop $ sudo bash install.sh
 	```
 
-	**Alternatively**, you can install the `systemd` service for the current user as shown below:
-
-	```bash
-	nvidia@agx-desktop $ mkdir -p ~/.config/systemd/user
-	nvidia@agx-desktop $ sudo cp jetson_stats_prometheus_collector.service /etc/systemd/system/
-	nvidia@agx-desktop $ systemctl --user daemon-reload 
-	nvidia@agx-desktop $ systemctl --user start jetson_stats_prometheus_collector
-	nvidia@agx-desktop $ systemctl --user status jetson_stats_prometheus_collector
-	```
-
-
-**Note:** The Prometheus metrics collector listens on port `8000` by default. If you wish to change this you will need to edit the `jetson_stats_prometheus_collector.service` file and change the `--port` argument to your required port number. You will also need to ensure to use the same port value later in this readme when referencing `REPLACEME_YOUR_JETSON_PROMETHEUS_COLLECTOR_PORT`.
-
-
-### Grafana Dashboard
-
-1. Install required Grafana plugins and restart the Grafana service
-
-	```bash
-	$ grafana-cli plugins install marcusolsson-dynamictext-panel
-	$ sudo systemctl restart grafana-server.service
-	```
+**Note:** The Prometheus metrics collector listens on port `8000` by default. If you wish to change this you will need to edit the `jetson_stats_prometheus_collector.service` file and change the `--port` argument to your required port number.
 
 ### Prometheus Metrics Collection
 
@@ -76,11 +43,8 @@ The following hardware and software should already be configured and operational
 	scrape_configs:
 	  - job_name: 'nvidia_jetson'
 	    static_configs:
-	    - targets: ['REPLACEME_YOUR_JETSON_HOST_IP:REPLACEME_YOUR_JETSON_PROMETHEUS_COLLECTOR_PORT']
+	    - targets: ['JETSON_HOST_IP:JETSON_PROMETHEUS_COLLECTOR_PORT']
 	```
-	
-	**Note:** Replace the `REPLACEME_XX` parts with the corresponding values.
-
 
 3. Restart the Prometheus service for the changes to take effect
 
@@ -93,7 +57,7 @@ The following hardware and software should already be configured and operational
 
 1. From the Grafana Admin Dashboard select `+Create` -> `Import`
 
-2. Open the file `jetson_stats_grafana_dashboard.json` and update the `__inputs` entry for the `DS_MY_PROMETHEUS` value and save the file
+2. Open the file `jetson_stats_grafana_dashboard.json` and change the `__inputs` entry for the `DS_MY_PROMETHEUS` value to your prometheus name and save the file
 
 3. Next, copy and paste the contents of modified `jetson_stats_grafana_dashboard.json` into the text box named `Import via panel json` or select the file using the `Upload JSON file` button
 
--- jetson_stats_prometheus_collector.py
+++ jetson_stats_prometheus_collector.py
@@ -22,173 +22,178 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import time
 import atexit
 import argparse
 from jtop import jtop, JtopException
-from prometheus_client.core import InfoMetricFamily, GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from prometheus_client.core import InfoMetricFamily, GaugeMetricFamily, REGISTRY
 from prometheus_client import start_http_server
 
 
-class CustomCollector(object):
+class JetsonNanoCollector(object):
     def __init__(self):
         atexit.register(self.cleanup)
+
         self._jetson = jtop()
         self._jetson.start()
+        if not self._jetson.ok():
+            raise JtopException("Jtop not available")
 
     def cleanup(self):
         print("Closing jetson-stats connection...")
         self._jetson.close()
 
     def collect(self):
-        if self._jetson.ok():
-            # print('emc: ', self._jetson.emc)
-            # print('iram: ', self._jetson.iram)
-            # print('mts: ', self._jetson.mts)
+        yield from self.collect_board_info()
+        yield from self.collect_system_uptime()
+        yield from self.collect_cpu_usage()
+        yield from self.collect_gpu_usage()
+        yield from self.collect_ram_usage()
+        yield from self.collect_disk_usage()
+        yield from self.collect_fan_usage()
+        yield from self.collect_sensor_temperatures()
+        yield from self.collect_power_usage()
+        yield from self.collect_network_interfaces()
 
-            #
-            # Board info 
-            #
-            i = InfoMetricFamily('jetson_info_board', 'Board sys info', labels=['board_info'])
-            i.add_metric(['info'], {
-                'machine': self._jetson.board['info']['machine'], 
-                'jetpack': self._jetson.board['info']['jetpack'], 
-                'l4t': self._jetson.board['info']['L4T']
-                })
+    def collect_board_info(self):
+        try:
+            i = InfoMetricFamily('jetson_board', 'Board sys info')
+            i.add_metric([],{
+                'machine': self._jetson.board['platform']['Machine'],
+                'distribution': self._jetson.board['platform']['Distribution'],
+                'release': self._jetson.board['platform']['Release'],
+                'jetpack': self._jetson.board['hardware']['Jetpack'], 
+                'l4t': self._jetson.board['hardware']['L4T'],
+                'module': self._jetson.board['hardware']['Module'],
+                'type': self._jetson.board['hardware']['Model'],
+                'codename': self._jetson.board['hardware']['Codename'],
+                'soc': self._jetson.board['hardware']['SoC'],
+                'cuda_arch_bin': self._jetson.board['hardware']['CUDA Arch BIN'],
+                'serial_number': self._jetson.board['hardware']['Serial Number'],
+                'nvpmode': self._jetson.nvpmodel.name
+            })
             yield i
+        except JtopException as e:
+            print("Jtop Error collecting board info: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            i = InfoMetricFamily('jetson_info_hardware', 'Board hardware info', labels=['board_hw'])                    
-            i.add_metric(['hardware'], {
-                'type': self._jetson.board['hardware']['TYPE'],
-                'codename': self._jetson.board['hardware']['CODENAME'],
-                'soc': self._jetson.board['hardware']['SOC'],
-                'chip_id': self._jetson.board['hardware']['CHIP_ID'],
-                'module': self._jetson.board['hardware']['MODULE'],
-                'board': self._jetson.board['hardware']['BOARD'],
-                'cuda_arch_bin': self._jetson.board['hardware']['CUDA_ARCH_BIN'],
-                'serial_number': self._jetson.board['hardware']['SERIAL_NUMBER']
-                })
-            yield i
-
-            #
-            # NV power mode
-            #
-            i = InfoMetricFamily('jetson_nvpmode', 'NV power mode', labels=['nvpmode'])
-            i.add_metric(['mode'], {'mode': self._jetson.nvpmodel.name})
-            yield i
-
-            #
-            # System uptime 
-            #
+    def collect_system_uptime(self):
+        try:
             g = GaugeMetricFamily('jetson_uptime', 'System uptime', labels=['uptime'])
             days = self._jetson.uptime.days
             seconds = self._jetson.uptime.seconds
-            hours = seconds//3600
-            minutes = (seconds//60) % 60
+            hours = seconds // 3600
+            minutes = (seconds // 60) % 60
             g.add_metric(['days'], days)
             g.add_metric(['hours'], hours)
             g.add_metric(['minutes'], minutes)
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting system uptime: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            #
-            # CPU usage 
-            #
+    def collect_cpu_usage(self):
+        try:
             g = GaugeMetricFamily('jetson_usage_cpu', 'CPU % schedutil', labels=['cpu'])
-            g.add_metric(['cpu_1'], self._jetson.cpu['CPU1']['val'])
-            g.add_metric(['cpu_2'], self._jetson.cpu['CPU2']['val'])
-            g.add_metric(['cpu_3'], self._jetson.cpu['CPU3']['val'])
-            g.add_metric(['cpu_4'], self._jetson.cpu['CPU4']['val'])
-            g.add_metric(['cpu_5'], self._jetson.cpu['CPU5']['val'])
-            g.add_metric(['cpu_6'], self._jetson.cpu['CPU6']['val'])
-            g.add_metric(['cpu_7'], self._jetson.cpu['CPU7']['val'])
-            g.add_metric(['cpu_8'], self._jetson.cpu['CPU8']['val'])
-            yield g
+            for i in range(len(self._jetson.cpu['cpu'])):
+                g.add_metric(['cpu_' + str(i + 1)], 100.0 - self._jetson.cpu['cpu'][i]['idle'])
 
-            # 
-            # GPU usage
-            #
+            g.add_metric(['cpu_total'], 100.0 - self._jetson.cpu['total']['idle'])
+            yield g
+        except JtopException as e:
+            print("Jtop Error collecting CPU usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
+
+    def collect_gpu_usage(self):
+        try:
             g = GaugeMetricFamily('jetson_usage_gpu', 'GPU % schedutil', labels=['gpu'])
-            g.add_metric(['val'], self._jetson.gpu['val'])
-            # g.add_metric(['frq'], self._jetson.gpu['frq'])
-            # g.add_metric(['min_freq'], self._jetson.gpu['min_freq'])
-            # g.add_metric(['max_freq'], self._jetson.gpu['max_freq'])
+            g.add_metric(['val'], self._jetson.gpu['gpu']['status']['load'])
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting GPU usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            # 
-            # RAM usage
-            #
-            g = GaugeMetricFamily('jetson_usage_ram', 'Memory usage', labels=['ram'])
-            g.add_metric(['used'], self._jetson.ram['use'])
-            g.add_metric(['shared'], self._jetson.ram['shared'])
-            # g.add_metric(['total'], self._jetson.ram['tot'])
-            # g.add_metric(['unit'], self._jetson.ram['unit'])
+    def collect_ram_usage(self):
+        try:
+            g = GaugeMetricFamily('jetson_usage_ram', 'Memory usage', labels=['device', 'type'])
+            g.add_metric(['ram', 'used'], self._jetson.memory['RAM']['used'])
+            g.add_metric(['ram', 'total'], self._jetson.memory['RAM']['tot'])
+            g.add_metric(['swap', 'used'], self._jetson.memory['SWAP']['used'])
+            g.add_metric(['swap', 'total'], self._jetson.memory['SWAP']['tot'])
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting RAM usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            # 
-            # Disk usage
-            #
+    def collect_disk_usage(self):
+        try:
             g = GaugeMetricFamily('jetson_usage_disk', 'Disk space usage', labels=['disk'])
             g.add_metric(['used'], self._jetson.disk['used'])
             g.add_metric(['total'], self._jetson.disk['total'])
             g.add_metric(['available'], self._jetson.disk['available'])
             g.add_metric(['available_no_root'], self._jetson.disk['available_no_root'])
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting disk usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            # 
-            # Fan usage
-            #
+    def collect_fan_usage(self):
+        try:
             g = GaugeMetricFamily('jetson_usage_fan', 'Fan usage', labels=['fan'])
-            g.add_metric(['speed'], self._jetson.fan['speed'])
-            # g.add_metric(['measure'], self._jetson.fan['measure'])
-            # g.add_metric(['auto'], self._jetson.fan['auto'])
-            # g.add_metric(['rpm'], self._jetson.fan['rpm'])
-            # g.add_metric(['mode'], self._jetson.fan['mode'])
+            g.add_metric(['speed'], self._jetson.fan['tegra_pwmfan']['speed'][0])
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting fan usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
-            # 
-            # Swapfile usage
-            #
-            g = GaugeMetricFamily('jetson_usage_swap', 'Swapfile usage', labels=['swap'])
-            g.add_metric(['used'], self._jetson.swap['use'])
-            g.add_metric(['total'], self._jetson.swap['tot'])
-            # g.add_metric(['unit'], self._jetson.swap['unit'])
-            # g.add_metric(['cached_size'], self._jetson.swap['cached']['size'])
-            # g.add_metric(['cached_unit'], self._jetson.swap['cached']['unit'])
+    def collect_sensor_temperatures(self):
+        try:
+            g = GaugeMetricFamily('jetson_temperatures', 'Sensor temperatures', labels=['device'])
+            g.add_metric(['ao'], self._jetson.temperature['AO']['temp'] if 'AO' in self._jetson.temperature else 0)
+            g.add_metric(['cpu'], self._jetson.temperature['CPU']['temp'] if 'CPU' in self._jetson.temperature else 0)
+            g.add_metric(['gpu'], self._jetson.temperature['GPU']['temp'] if 'GPU' in self._jetson.temperature else 0)
+            g.add_metric(['pll'], self._jetson.temperature['PLL']['temp'] if 'PLL' in self._jetson.temperature else 0)
+            g.add_metric(['thermal'], self._jetson.temperature['thermal']['temp'] if 'thermal' in self._jetson.temperature else 0)
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting sensor temperatures: " + str(e))
 
-            # 
-            # Sensor temperatures
-            #
-            g = GaugeMetricFamily('jetson_temperatures', 'Sensor temperatures', labels=['temperature'])
-            g.add_metric(['ao'], self._jetson.temperature['AO'] if 'AO' in self._jetson.temperature else 0)
-            g.add_metric(['gpu'], self._jetson.temperature['GPU'] if 'GPU' in self._jetson.temperature else 0)
-            g.add_metric(['tdiode'], self._jetson.temperature['Tdiode'] if 'Tdiode' in self._jetson.temperature else 0)
-            g.add_metric(['aux'], self._jetson.temperature['AUX'] if 'AUX' in self._jetson.temperature else 0)
-            g.add_metric(['cpu'], self._jetson.temperature['CPU'] if 'CPU' in self._jetson.temperature else 0)
-            g.add_metric(['thermal'], self._jetson.temperature['thermal'] if 'thermal' in self._jetson.temperature else 0)
-            g.add_metric(['tboard'], self._jetson.temperature['Tboard'] if 'Tboard' in self._jetson.temperature else 0)
-            yield g
-
-            # 
-            # Power
-            #
+    def collect_power_usage(self):
+        try:
             g = GaugeMetricFamily('jetson_usage_power', 'Power usage', labels=['power'])
-            g.add_metric(['cpu'], self._jetson.power[1]['CPU']['cur'] if 'CPU' in self._jetson.power[1] else 0)
-            g.add_metric(['cv'], self._jetson.power[1]['CV']['cur'] if 'CV' in self._jetson.power[1] else 0)
-            g.add_metric(['gpu'], self._jetson.power[1]['GPU']['cur'] if 'GPU' in self._jetson.power[1] else 0)
-            g.add_metric(['soc'], self._jetson.power[1]['SOC']['cur'] if 'SOC' in self._jetson.power[1] else 0)
-            g.add_metric(['sys5v'], self._jetson.power[1]['SYS5V']['cur'] if 'SYS5V' in self._jetson.power[1] else 0)
-            g.add_metric(['vddrq'], self._jetson.power[1]['VDDRQ']['cur'] if 'VDDRQ' in self._jetson.power[1] else 0)
+            g.add_metric(['in'], self._jetson.power['tot']['power'])
+            g.add_metric(['cpu'], self._jetson.power['rail']['POM_5V_CPU']['power'])
+            g.add_metric(['gpu'], self._jetson.power['rail']['POM_5V_GPU']['power'])
             yield g
+        except JtopException as e:
+            print("Jtop Error collecting power usage: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
+
+    def collect_network_interfaces(self):
+        try:
+            i = InfoMetricFamily('jetson_network', 'Network info', labels=['interface_name'])
+            for netName, ip in self._jetson.local_interfaces['interfaces'].items():
+                i.add_metric([netName], {'ip_address': ip})
+            yield i
+        except JtopException as e:
+            print("Jtop Error collecting network interfaces: " + str(e))
+        except Exception as e:
+            print("Error collecting board info: " + str(e))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=8000, help='Metrics collector port number')
-
     args = parser.parse_args()
 
     start_http_server(args.port)
-    REGISTRY.register(CustomCollector())
-    while True:
-        time.sleep(1)
+    REGISTRY.register(JetsonNanoCollector())
+
+    input()
