461d9f25daa135f0927f6036a225fd81c359fa02
Amirali.T <kebritam@proton.me>
2024-02-19 14:45:01 +0330

    @@ fix bash script

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ sudo cp jetson_stats_prometheus_collector.py /usr/local/bin/
 
 # Install service for the current user
 mkdir -p ~/.config/systemd/user
-cp jetson_stats_prometheus_collector.service ~/.config/systemd/user/
-systemctl --user daemon-reload
-systemctl --user start jetson_stats_prometheus_collector
-systemctl --user status jetson_stats_prometheus_collector
+
+cp jetson_stats_prometheus_collector.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start jetson_stats_prometheus_collector
+systemctl status jetson_stats_prometheus_collector
